69e77b35eb6f31f78eee6dde1d98d7826bd40497
M0hc3n <mouhcen.chouireb@ensia.edu.dz>
2025-02-04 19:28:03 +0100

    added main inception arch, still need to test it

--- Inception/V2/src/modeling/auxilary.py
+++ Inception/V2/src/modeling/auxilary.py
@@ -1,6 +1,6 @@
 from torch import nn, flatten
 
-import torch.nn.functional as F 
+import torch.nn.functional as F
 
 from modeling.conv_block import ConvBlock
 
@@ -15,15 +15,15 @@ class Auxiliary(nn.Module):
         self.fc2 = nn.Linear(1024, num_classes)
 
     def forward(self, x):
-        x = F.adaptive_avg_pool2d(x, (5, 5)) 
-        
+        x = F.adaptive_avg_pool2d(x, (5, 5))
+
         x = self.conv(x)
-        
+
         x = flatten(x, 1)
-        
+
         x = F.relu(self.fc1(x), inplace=True)
-        
+
         x = F.dropout(x, 0.7, training=self.training)
-        
+
         x = self.fc2(x)
         return x
